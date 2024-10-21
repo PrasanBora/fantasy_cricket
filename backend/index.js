616c922ed02e806020f0d3aba33ac74bf52c0c46
PrasanBora <prasanbora23@gmail.com>
2024-10-22 02:11:25 +0530

    commit7

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 // Start the server
